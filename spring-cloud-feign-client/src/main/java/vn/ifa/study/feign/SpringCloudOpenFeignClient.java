fcbdb37e199687508fdbcf6dd8c09981682bd068
Minh Trần <minh.tran@spsglobal.com>
2022-12-29 14:15:43 +0700

    Configure retryer

--- spring-cloud-feign-client/src/main/java/vn/ifa/study/feign/SpringCloudOpenFeignClient.java
+++ spring-cloud-feign-client/src/main/java/vn/ifa/study/feign/SpringCloudOpenFeignClient.java
@@ -3,6 +3,9 @@ package vn.ifa.study.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import feign.Retryer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -13,4 +16,9 @@ public class SpringCloudOpenFeignClient {
         SpringApplication.run(SpringCloudOpenFeignClient.class, args);
     }
 
+    @Bean
+    Retryer retryer() {
+
+        return new Retryer.Default();
+    }
 }
